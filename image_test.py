03d009345b0c0834469671cac33347cf7ae50a4a
Rohit Nag <rnag2018@gmail.com>
2023-04-24 20:11:41 +0100

    removed thresholding windows

---
+++ image_test.py
@@ -0,0 +1,11 @@
+import cv2 as cv
+
+
+path_img = cv.imread('path_binary_image.jpg')
+
+
+def get_next_pixel_coordinate(start_coordinate, slope):
+
+
+def ray_trace(start_coordinate, slope, image):
+    ...
